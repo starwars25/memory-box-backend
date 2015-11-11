caf252b92e9fb5fce49899839e8d568fb68db2b2
Alexander <a.starwars.d@gmail.com>
2015-11-11 21:38:55 +0200

    Implements user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :last_name, presence: true
+  validates :password_digest, presence: true
+
+  def generate_password(password)
+    if password.length < 8
+      raise 'Password too short'
+    end
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end

561cc41d0dfd7da444d8ad98bbec1560a731ec00
Alexander <a.starwars.d@gmail.com>
2015-11-11 21:35:36 +0200

    Writes user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate presence of name" do
+    before = User.count
+    user = User.new(name: '', last_name: 'bar', email: 'foobar@foo.bar')
+    user.generate_password('nice_password')
+    user.save
+    expect(User.count).to eql(before)
+  end
+
+  it "should validate presence of email" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: 'bar', email: '')
+    user.generate_password('nice_password')
+    user.save
+    expect(User.count).to eql(before)
+  end
+
+  it "should validate uniqueness of email" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: 'bar', email: 'foobar@bar.com')
+    user.generate_password('nice_password')
+    user.save
+    expect(User.count).to eql(before + 1)
+
+    user = User.new(name: 'foo', last_name: 'bar', email: 'foobar@bar.com')
+    user.generate_password('nice_password')
+    user.save
+    expect(User.count).to eql(before + 1)
+
+  end
+
+
+  it "should validate presence of last name" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: '', email: 'foobar@foo.bar')
+    user.generate_password('nice_password')
+    user.save
+    expect(User.count).to eql(before)
+  end
+
+  it "should validate length of password" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: 'bar', email: 'foobar@foo.bar')
+    expect {user.generate_password('foo')}.to raise_error 'Password too short'
+    user.save
+    expect(User.count).to eql(before)
+  end
+
+  it "should validate presence of password" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: 'bar', email: 'foobar@foo.bar')
+    user.save
+    expect(User.count).to eql(before)
+  end
+
+  it "should create user" do
+    before = User.count
+    user = User.new(name: 'foo', last_name: 'bar', email: 'foobar@foo.bar')
+    user.generate_password 'nice_password'
+    user.save
+    expect(User.count).to eql(before + 1)
+
+  end
+
+
+
 end

c5b80c81a6e4c4f9d6a710d22271e8c0c96e1e2c
Alexander <a.starwars.d@gmail.com>
2015-11-18 20:07:13 +0200

    Refactored tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   before(:all) do
+    clean
     create_users
 
   end
@@ -9,15 +10,15 @@ RSpec.describe UsersController, type: :controller do
   it "should get index" do
     get :index
     json = JSON.parse @response.body
-    expect(json[0]['name']).to eql('Sanya')
-    expect(json[1]['name']).to eql('Fedor')
+    expect(json[0]['name']).to eql(@first.name)
+
+    expect(json[1]['name']).to eql(@second.name)
   end
 
   it "should get sanya" do
-    get :show, {id: 1}
+    get :show, {id: @first.id}
     json = JSON.parse @response.body
-    expect(json['name']).to eql('Sanya')
-    expect(json['last_name']).to eql('Bogomolov')
+    expect(json['name']).to eql(@first.name)
   end
 
   it "should get nothing" do
@@ -27,29 +28,30 @@ RSpec.describe UsersController, type: :controller do
   end
 
   it "should update user" do
-    put :update, {id: 1, user: {name: 'Alexander'}}, format: :json
+    put :update, {id: @first.id, user: {name: 'Alexander'}}, format: :json
     json = JSON.parse @response.body
     expect(json['result']).to eql('success')
-    expect(User.find(1).name).to eql('Alexander')
+    expect(User.find(@first.id).name).to eql('Alexander')
   end
 
   it "should update invalid user" do
     put :update, {id: 3, user: {name: 'Alexander'}}, format: :json
     json = JSON.parse @response.body
     expect(json['error']).to eql('no such user')
-    expect(User.find(1).name).to eql('Sanya')
+    expect(User.find(@first.id).name).to eql(@first.name)
   end
 
   it "should not update user" do
-    put :update, {id: 1, user: {name: ''}}, format: :json
+    name = @first.name
+    put :update, {id: @first.id, user: {name: ''}}, format: :json
     json = JSON.parse @response.body
     expect(json['error']).to eql('invalid data')
-    expect(User.find(1).name).to eql('Sanya')
+    expect(User.find(@first.id).name).to eql(name)
   end
 
   it "should delete user" do
     before = User.count
-    delete :destroy, {id: 1}
+    delete :destroy, {id: @first.id}
     json = JSON.parse @response.body
     expect(json['result']).to eql('success')
     expect(User.count).to eql(before - 1)
@@ -65,7 +67,7 @@ RSpec.describe UsersController, type: :controller do
 
   it "should create user" do
     before = User.count
-    post :create, {user: {name: 'Den', last_name: 'Krasava', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'great_password'}}
+    post :create, {user: {name: 'Den Krasava', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'great_password'}}
     json = JSON.parse @response.body
     expect(json['result']).to eql('success')
     expect(User.count).to eql(before + 1)
@@ -76,7 +78,7 @@ RSpec.describe UsersController, type: :controller do
 
   it "should not create user" do
     before = User.count
-    post :create, {user: {name: '', last_name: 'Krasava', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'great_password'}}
+    post :create, {user: {name: '', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'great_password'}}
     json = JSON.parse @response.body
     expect(json['error']).to eql('invalid params')
     expect(User.count).to eql(before)
@@ -84,7 +86,7 @@ RSpec.describe UsersController, type: :controller do
 
   it "password do not match" do
     before = User.count
-    post :create, {user: {name: 'Den', last_name: 'Krasava', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'nice_password'}}
+    post :create, {user: {name: 'Den Krasava', email: 'notpidor@gmai.com', password: 'great_password', password_confirmation: 'nice_password'}}
     json = JSON.parse @response.body
     expect(json['error']).to eql('passwords do not match')
     expect(User.count).to eql(before)
@@ -92,7 +94,7 @@ RSpec.describe UsersController, type: :controller do
 
   it "invalid password" do
     before = User.count
-    post :create, {user: {name: 'Den', last_name: 'Krasava', email: 'notpidor@gmai.com', password: 'foo', password_confirmation: 'foo'}}
+    post :create, {user: {name: 'Den Krasava', email: 'notpidor@gmai.com', password: 'foo', password_confirmation: 'foo'}}
     json = JSON.parse @response.body
     expect(json['error']).to eql('invalid password')
     expect(User.count).to eql(before)

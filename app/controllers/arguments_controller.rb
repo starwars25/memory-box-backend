909a08cce3e7721c4f9a482ef1bd6d9c25f69770
Alexander <a.starwars.d@gmail.com>
2015-11-21 10:44:24 +0200

    Adds create argument

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -23,10 +23,32 @@ class ArgumentsController < ApplicationController
     end
   end
 
+  def create
+    box = Box.find_by(id: params[:argument][:box_id])
+    if box
+      if box.users.include?(current_user)
+        argument = Argument.new(argument_params)
+        if argument.save
+          render json: {result: 'success'}
+        else
+          render json: {error: 'invalid params'}
+        end
+      else
+        render json: {error: 'wrong user'}
+      end
+    else
+      render json: {error: 'no such box'}
+    end
+  end
+
   private
 
   def logged_in
     render json: {error: 'not logged in'} unless current_user
+  end
+
+  def argument_params
+    params.require(:argument).permit(:title, :description, :box_id, :expires)
   end
 
 
--- spec/controllers/arguments_controller_spec.rb
+++ spec/controllers/arguments_controller_spec.rb
@@ -45,4 +45,59 @@ RSpec.describe ArgumentsController, type: :controller do
 
   end
 
+  it "test create" do
+    # not logged in
+
+    post :create, argument: {title: 'Title', description: 'Description', expires: 14.days.from_now, box_id: @first_box.id}, format: :json
+    json = JSON.parse @response.body
+    expect(json['error']).to eql 'not logged in'
+
+
+    # not a member of box
+
+    @token = log_in @third
+    @request.headers['user-id'] = @third.id
+    @request.headers['token'] = @token
+    post :create, argument: {title: 'Title', description: 'Description', expires: 14.days.from_now, box_id: @first_box.id}, format: :json
+    json = JSON.parse @response.body
+    expect(json['error']).to eql 'wrong user'
+
+    # invalid params
+
+    @token = log_in @first
+    @request.headers['user-id'] = @first.id
+    @request.headers['token'] = @token
+    post :create, argument: {description: 'Description', expires: 14.days.from_now, box_id: @first_box.id}, format: :json
+    json = JSON.parse @response.body
+    expect(json['error']).to eql 'invalid params'
+
+    # no such box
+
+    @token = log_in @first
+    @request.headers['user-id'] = @first.id
+    @request.headers['token'] = @token
+    post :create, argument: {title: 'Title', description: 'Description', expires: 14.days.from_now, box_id: 100}, format: :json
+    json = JSON.parse @response.body
+    expect(json['error']).to eql 'no such box'
+
+    # successful
+
+    @token = log_in @first
+    @request.headers['user-id'] = @first.id
+    @request.headers['token'] = @token
+    before_first_user = @first.arguments.count
+    before_second_user = @second.arguments.count
+    before_first_box = @first_box.arguments.count
+    post :create, argument: {title: 'Title', description: 'Description', expires: 14.days.from_now, box_id: @first_box.id}, format: :json
+    json = JSON.parse @response.body
+    expect(json['result']).to eql 'success'
+    @first.reload
+    @second.reload
+    @first_box.reload
+
+    expect(@first.arguments.count).to eql(before_first_user + 1)
+    expect(@second.arguments.count).to eql(before_second_user + 1)
+    expect(@first_box.arguments.count).to eql(before_first_box + 1)
+  end
+
 end

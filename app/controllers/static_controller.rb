8fd173ee8e1f8d0c1530feef35fe0704030d988c
Alexander <a.starwars.d@gmail.com>
2015-11-22 19:13:24 +0200

    Adds developer authorization

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,32 @@
 class StaticController < ApplicationController
+  include StaticHelper
+  before_action :is_developer, only: [:index]
+  before_action :already_developer, only: [:login, :sign_in]
+
   def index
 
   end
+
+  def sign_in
+
+  end
+
+  def login
+    if params[:session][:password] == 'MemoryBox'
+      session['logged_in'] = true
+      redirect_to root_url
+    else
+      render 'static/sign_in'
+    end
+  end
+
+  def is_developer
+    redirect_to login_path unless current_developer
+  end
+
+  def already_developer
+    redirect_to root_url if current_developer
+  end
+
+
 end
--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,5 @@
 module StaticHelper
+  def current_developer
+    session['logged_in'] == true
+  end
 end

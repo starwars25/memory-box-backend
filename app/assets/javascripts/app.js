d6c6fec3ffcedc5d5a699b38b2742688be321bea
Alexander <a.starwars.d@gmail.com>
2015-12-21 00:40:18 +0200

    Adds more angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,22 +2,44 @@
     var app = angular.module('MemoryBox', ['ngRoute']);
 
 
-    app.controller('LoginCtrl', [function () {
-        this.testVariable = 'Hello, World';
+    app.controller('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.submitForm = function() {
+            var config = {
+                method: 'POST',
+                url: '/authentication',
+                data: {
+                    user: {
+                        email: $scope.userEmail,
+                        password: $scope.userPassword
+
+                    }
+                },
+                headers: {
+                    "content-type": 'application/json'
+                }
+            };
+            $http(config).then(function success(response) {
+                if (response.status === 201) {
+                    console.log(response.data);
+                }
+            }, function error(response) {});
+        };
+        $scope.userEmail = '';
+        $scope.userPassword = '';
     }]);
 
     app.controller('RegisterCtrl', [function () {
-        this.testVariable = 'Ты пидор';
     }]);
 
-    app.controller('MainCtrl', [function() {
+    app.controller('MainCtrl', ['$location', function($location) {
 
     }]);
 
     app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $routeProvider.when('/login', {
             templateUrl: 'templates/login.html',
-            controller: 'LoginCtrl'
+            controller: 'LoginCtrl',
+            controllerAs: 'login'
         });
         $routeProvider.when('/register', {
             templateUrl: 'templates/register.html',
@@ -25,8 +47,9 @@
         });
         $routeProvider.when('/', {
             templateUrl: 'templates/main.html',
-            controller: 'MainCtrl'
-        })
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        });
         $routeProvider.otherwise({
             redirectTo: '/'
         })

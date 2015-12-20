4961189b5720481e4be210e64d10798b3ababe80
Alexander <a.starwars.d@gmail.com>
2015-12-20 19:31:48 +0200

    Little improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:id]).includes(:boxes, :arguments)
     # byebug
     if user
       if current_user && current_user.id == params[:id].to_i
